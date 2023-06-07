21f56e0a7fc5f7af48c7cd0159886b4a8189b55f
erpang <hnzhrh@gmail.com>
2023-06-07 22:09:43 +0800

    [feature] Remove unused code.

--- main.c
+++ main.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-#include<string.h>
 #include "data-structure/sds.h"
-
-void testSds() {
-    printf("sds8 length:%lu\n", sizeof(struct sdshdr8));
-    printf("sds16 length:%lu\n", sizeof(struct sdshdr16));
-    printf("sds32 length:%lu\n", sizeof(struct sdshdr32));
-    printf("sds64 length:%lu\n", sizeof(struct sdshdr64));
-}
 
 sds testCreateSds(char *s) {
     return sdsnew(s);
 }
 
 int main() {
-//    testSds();
     sds str = testCreateSds("abcdefghijklasdfaasdfasdfasdfasdfasdfasdfasdfasdfasdfa");
     printf("sds:%s\n", str);
     return 0;
